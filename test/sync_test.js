60d5d9bb744f7372eaa114247569a1ef22c6a224
AJ Ostrow <alexander.ostrow@gmail.com>
2015-01-16 07:02:15 -0500

    no early return

--- test/sync_test.js
+++ test/sync_test.js
@@ -13,7 +13,7 @@ var sync = require('../src/sync')
 describe('sync', function () {
   it('should create migrations table', function * () {
     var structs = yield parse.dir(__dirname + '/migrations')
-    return yield sync(db, structs)
+    yield sync(db, structs)
     var rows = yield db.exec('select count(*) from migrations')
     assert.equal('2', rows[0].count)
   })
